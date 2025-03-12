4f02909a0039d65aa0cb5133cab477426dafba95
sagaramin95 <Sagar.Amin@techconsulting.tech>
2025-03-12 18:01:55 -0400

    Initial Commit

---
+++ SwiftUIRepository/SwiftUIRepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRepositoryApp.swift
+//  SwiftUIRepository
+//
+//  Created by Sagar Amin on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIRepositoryTests/SwiftUIRepositoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRepositoryTests.swift
+//  SwiftUIRepositoryTests
+//
+//  Created by Sagar Amin on 3/12/25.
+//
+
+import Testing
+@testable import SwiftUIRepository
+
+struct SwiftUIRepositoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
